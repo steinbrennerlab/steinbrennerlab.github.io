e98e18a1a4e5dd1b77e54c8afc3c79033fc0b979
adsteinbrenner <42748811+adsteinbrenner@users.noreply.github.com>
2019-05-02 14:51:37 -0700

    baseurl /

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "News item #1",
         "excerpt":"","categories": [],
         "tags": [],
-        "url": "https://steinbrennerlab.org/_site/news/2019-05-02-launch.html"
+        "url": "https://steinbrennerlab.org/news/2019-05-02-launch.html"
       },{
         "title": "Lab website launched",
         "excerpt":"The lab website is up and running!  Check back for updates ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "https://steinbrennerlab.org/_site/news/2019-05-01-launch.html"
+        "url": "https://steinbrennerlab.org/news/2019-05-01-launch.html"
       },{
         "title": "Chocolate Chip Cookies",
         "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
         "tags": [],
-        "url": "https://steinbrennerlab.org/_site/people/steinbrenner.html"
+        "url": "https://steinbrennerlab.org/people/steinbrenner.html"
       },{
         "title": "Lab website launched",
         "excerpt":"The lab website is up and running!  Check back for updates ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "https://steinbrennerlab.org/_site/jekyll/update/2019/05/02/launch.html"
+        "url": "https://steinbrennerlab.org/jekyll/update/2019/05/02/launch.html"
       }]
