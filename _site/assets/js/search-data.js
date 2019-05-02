a9828f1229604ed1b436f2d819975bcc9c9add5c
adsteinbrenner <42748811+adsteinbrenner@users.noreply.github.com>
2019-05-02 11:32:50 -0700

    Initial commit

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Lab website launched",
+        "excerpt":"The lab website is up and running!  Check back for updates ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2019/05/02/launch.html"
+      }]
