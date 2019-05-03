6767a05888a35311b675292e935bd30e0551f2cc
adsteinbrenner <42748811+adsteinbrenner@users.noreply.github.com>
2019-05-02 17:09:24 -0700

    another update

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Adam Steinbrenner, Principal Investigator",
         "excerpt":"Adam has been an Assistant Professor in the Department of Biology since 2019 Education PhD, Plant Biology University of California, Berkeley BS, Biology Tufts University Other Other information ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/steinbrenner.html"
+        "url": "https://steinbrennerlab.org/people/steinbrenner.html"
       },{
         "title": "Lab website launched",
         "excerpt":"The lab website is up and running!  Check back for updates ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/05/02/launch.html"
+        "url": "https://steinbrennerlab.org/2019/05/02/launch.html"
       }]
