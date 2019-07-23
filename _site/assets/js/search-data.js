0026b90e10c79e20ce408203ca02398dcc97b630
adsteinbrenner <42748811+adsteinbrenner@users.noreply.github.com>
2019-07-23 16:16:21 -0700

    post

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -13,4 +13,14 @@ var store = [{
         "excerpt":"We are hiring a research technologist to take on a variety of research and management roles as the lab gets going.  Please see UW Hires for the official job ad and here for the flyer (including how to apply) ","categories": [],
         "tags": [],
         "url": "https://steinbrennerlab.org/2019/05/13/technician.html"
+      },{
+        "title": "Inceptin Receptor Preprint is Live",
+        "excerpt":"The core of Adam’s postdoctoral work is now posted on bioRxiv (https://www.biorxiv.org/content/10.1101/679803v1) in a manuscript entitled “A Receptor for Herbivore-Associated Molecular Patterns Mediates Plant Immunity.” We are excited to connect surface immune recognition to chewing herbivory through INR. We’re very happy it’s available on bioRxiv as it works its way...","categories": [],
+        "tags": [],
+        "url": "https://steinbrennerlab.org/2019/07/03/inr_manuscipt.html"
+      },{
+        "title": "Summer conferences: MPMI, ASPB",
+        "excerpt":"We will present our INR preprint and other transcriptomic and genomic follow-ups at MPMI in Glasgow and ASPB in San Jose Aug 3-7 https://aspb.org/event/plant-biology-2019/  ","categories": [],
+        "tags": [],
+        "url": "https://steinbrennerlab.org/2019/07/23/conferences.html"
       }]
