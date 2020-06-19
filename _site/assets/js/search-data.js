f9e05b8e62f41047c795d2d257b1f274425f08fb
Adam Steinbrenner <42748811+steinbrennerlab@users.noreply.github.com>
2020-06-19 16:34:23 -0700

    remove italic

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,55 +2,55 @@ var store = [{
         "title": "Adam Steinbrenner, Principal Investigator",
         "excerpt":"astein10 /at/ uw.edu Adam Steinbrenner joined the UW Department of Biology in September 2019. Curriculum vitae – Steinbrenner_CV.pdf Experience HHMI and UC President’s Postdoctoral Fellow, University of California, San Diego (2016-2019) PhD, Plant Biology University of California, Berkeley BS, Biology Tufts University Projects I am fascinated by the plant immune...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/steinbrenner.html"
+        "url": "https://steinbrennerlab.org/people/steinbrenner.html"
       },{
         "title": "Antonio Chaparro, Research Technologist",
         "excerpt":"chaparro /at/ uw.edu   Tonio joined the lab in September 2019 after graduating from UW where he studied Molecular Biology and Environmental Science. He’s excited to continue growing plants and studying their complex immune systems! He also enjoys gardening, backpacking, and basketball. ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/chaparro.html"
+        "url": "https://steinbrennerlab.org/people/chaparro.html"
       },{
         "title": "Anthony Garcia, Undergraduate Researcher",
         "excerpt":"  Anthony is an undergraduate studying Plant Biology who joined the lab in October 2019. He loves learning about plant physiology, especially how plants respond to herbivores and pathogens, and also enjoys running, finding interesting music, and eating Polish and Mexican food. ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/garcia.html"
+        "url": "https://steinbrennerlab.org/people/garcia.html"
       },{
         "title": "Ava Kloss-Schmidt, Undergraduate Researcher",
         "excerpt":"Ava is an undergraduate studying Plant Biology at UW. She joined the Steinbrenner lab in October 2019 and is excited to learn more about working with plants in a lab setting. Her favorite place in Seattle is the Volunteer Park Conservatory and she would spend all her time in a...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/kloss-schmidt.html"
+        "url": "https://steinbrennerlab.org/people/kloss-schmidt.html"
       },{
         "title": "Simon Snoeck, Postdoctoral Researcher",
         "excerpt":"spsnoeck /at/ uw.edu Experience BS, MS and Doctor of Bioscience Engineering, Ghent university Interests My PhD research focused on genetic and molecular mechanisms of pesticide resistance and long-term host plant acclimation of the generalist herbivore Tetranychus urticae. Throughout my studies, my interests gradually evolved and extended beyond arthropods. I am...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/people/snoeck.html"
+        "url": "https://steinbrennerlab.org/people/snoeck.html"
       },{
         "title": "Lab website launched",
         "excerpt":"The lab website is up and running!  Check back for updates ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/05/02/launch.html"
+        "url": "https://steinbrennerlab.org/2019/05/02/launch.html"
       },{
         "title": "Hiring Research Technologist 1",
         "excerpt":"We are hiring a research technologist to take on a variety of research and management roles as the lab gets going.  Please see UW Hires for the official job ad and here for the flyer (including how to apply) ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/05/13/technician.html"
+        "url": "https://steinbrennerlab.org/2019/05/13/technician.html"
       },{
         "title": "Inceptin Receptor Preprint is Live",
         "excerpt":"The core of Adam’s postdoctoral work is now posted on bioRxiv (https://www.biorxiv.org/content/10.1101/679803v1) in a manuscript entitled “A Receptor for Herbivore-Associated Molecular Patterns Mediates Plant Immunity.” We are excited to connect surface immune recognition to chewing herbivory through INR. We’re very happy it’s available on bioRxiv as it works its way...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/03/inr_manuscipt.html"
+        "url": "https://steinbrennerlab.org/2019/07/03/inr_manuscipt.html"
       },{
         "title": "Summer conferences: MPMI, ASPB",
         "excerpt":"We will present our INR preprint and other transcriptomic and genomic follow-ups at MPMI in Glasgow and ASPB in San Jose Aug 3-7 https://aspb.org/event/plant-biology-2019/  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/07/23/conferences.html"
+        "url": "https://steinbrennerlab.org/2019/07/23/conferences.html"
       },{
         "title": "Antonio Chaparro joins the lab",
         "excerpt":"The lab’s first official member has joined!  Antonio will join as a technician.  Heis a recent graduate of the UW and comes in with experience in plant molecular genetics from his undergraduate research.  Welcome Antonio! ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/09/03/antonio_joins.html"
+        "url": "https://steinbrennerlab.org/2019/09/03/antonio_joins.html"
       },{
         "title": "Fall conference: APACE",
         "excerpt":"Prof. Steinbrenner will attend the 10th Conference of the Asia-Pacific Association of Chemical Ecologists in Hangzhou, China. He will present in Session 3: “Signaling and Perception in Plant-Herbivore Interactions.” Say hello if you are also attending, and come to hear about INR and our future plans to understand evolution of...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2019/09/13/APACE_2019.html"
+        "url": "https://steinbrennerlab.org/2019/09/13/APACE_2019.html"
       }]
